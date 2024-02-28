cd50d3239ed0c748e4b2370e43eb408eb9c4b2bb
sunweiqiang <sunweiqiang@yiwei.com>
2024-02-28 17:50:01 +0800

    增加json转换示例

--- object2json.go
+++ object2json.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+type Person struct {
+	Name string `json:"name"` // 指定字段名称为 "name"
+	Age  int    `json:"age"`
+}
+
+func main() {
+	p := Person{
+		Name: "Alice",
+		Age:  25,
+	}
+
+	// 将Person对象转换为JSON字符串
+	data, err := json.Marshal(p)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data))
 }
